bf622c8fdb22fba1cfb76b7414a4d409436e126c
Kth <vcro12123@naver.com>
2022-04-29 02:36:41 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from './pages/Main';
 function App() {
   return (
     <RecoilRoot>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/Main' element={<Main />} />
